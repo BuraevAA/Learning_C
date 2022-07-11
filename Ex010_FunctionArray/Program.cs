ede17f02bb8e6d879bd6228664308e5cbc15ce4e
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-07-11 21:50:49 +0300

    Добавили проект Ex010_FunctionArray

---
+++ Ex010_FunctionArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {111, 22, 33, 54, 33, 652, 76, 824};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index=index+1;
+    index++;
+}
