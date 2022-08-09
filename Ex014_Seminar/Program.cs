0d213137c2f3007c4a1f403671550101d6ff7a45
aa-buraev@bk.ru <aa-buraev@bk.ru>
2022-08-09 16:57:18 +0300

    Добавили проекты Ех014 и Ех015

---
+++ Ex014_Seminar/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+int[] list = new int[13];
+string result = String.Empty;
+
+for (int i = 0; i < list.Length; i++)
+{
+    list[i] = new Random().Next(1, 20);
+}
+
+void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.Write($"{col[position]} ");
+                position++;
+            }
+    }
+PrintArray(list);
+Console.WriteLine();
+for (int i = 0; i < list.Length / 2; i++)
+{
+    result += $"{list[i] * list[list.Length - i - 1]}, ";
+}
+if (list.Length % 2 != 0) result += $"{list[list.Length / 2]}";
+
+Console.WriteLine(result);
+
+
+
+
+
+
---
+++ Ex015_RecursionAlgorithm/Program.cs
@@ -0,0 +1,110 @@
+﻿Console.Clear();
+//string[,] table = new string[4, 5];
+//String.Empty
+//table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+//table[1,0] table[1,1] table[1,2] table[1,3] table[1,4]  
+/*table[1, 2] = "слово";
+for (int rows = 0; rows < table.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < table.GetLength(1); columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+    Console.WriteLine();
+}*/
+
+/*void FillPrintArray(int[,] matr)
+{
+        for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillPrintArray(matrix);*/
+
+/*void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] pic = new int[,]
+{
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+ {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+ {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+FillImage(13,13);
+PrintImage(pic);*/
+
+/*double Factorial (int n)
+{
+    //1!=1
+    //0!=1
+    if(n==1) return 1;
+    else return n*Factorial(n-1);
+}
+for (int i=1; i<40;i++)
+{
+    Console.WriteLine($"{i}!={Factorial(i)}");
+}*/
+
+//f(1)=1;
+//f(2)=1;
+//f(n)=f(n-1)+f(n-2)
+
+double Fib(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fib(n - 1) + Fib(n - 2);
+}
+for (int i=1; i<50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fib(i)}");
+}
